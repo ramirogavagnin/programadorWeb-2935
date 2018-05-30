e85d63f6902635180c015e315e659bb318bb9f56
Ramiro Gavagnin <ramirogavagnin@gmail.com>
2018-05-30 00:19:21 -0300

    javascript clase 01

---
+++ Clase 01/Alumno/Ejercicio/js/index.js
@@ -0,0 +1,65 @@
+
+
+
+// Name
+ var name
+
+ name = prompt('Tell me your name please')
+
+ console.log('Hi! ' + name)
+
+// Add
+
+ var numberClient
+
+ numberClient= prompt('Tell me a number')
+
+ 
+ var parsedValue = parseInt(numberClient, 10)
+
+ var numberSum = 2
+
+ var sumResult = parsedValue + numberSum
+
+ console.log(sumResult)
+
+ // Subtraction
+
+
+  var numberClient2
+
+ numberClient2= prompt('Tell me a number')
+
+ 
+ var parsedValue = parseInt(numberClient2, 10)
+
+ var numberSub = 1
+
+ var subResult = parsedValue - numberSub
+
+ console.log(subResult)
+
+
+// Add 2 var
+
+var numberClient3
+
+ numberClient3= prompt('Tell me a number')
+
+
+ var numberClient4
+
+ numberClient4= prompt('Tell me a another number')
+
+
+ var parsedValue1 = parseInt(numberClient3, 10)
+ 
+ var parsedValue2 = parseInt(numberClient4, 10)
+
+
+
+ var addResult = parsedValue1 + parsedValue2
+
+ console.log(addResult)
+
+ 
