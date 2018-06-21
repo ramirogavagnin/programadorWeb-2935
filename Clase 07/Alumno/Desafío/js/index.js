ed4b6dea4198de3330d270d9e63d2a02d88c2291
Ramiro Gavagnin <ramirogavagnin@gmail.com>
2018-06-20 22:55:18 -0300

    desafío 7

---
+++ Clase 07/Alumno/Desafío/js/index.js
@@ -0,0 +1,40 @@
+console.log('desafío 07')
+
+
+var studentsList = ['CARLOS','GERONIMO','NICOLAS','LUCAS','MARIA','FEDERICO','ANTONIO','LORNA','JULIAN','DIEGO','DANIELA','JUAN','MATEO','BARBARA','AGUSTIN','MARIO','MARIEL','ANA','FLORENCIA']
+
+
+ function saveLocalList (key, array){
+
+    if (typeof key === 'string' &&  Array.isArray(array)){
+
+        var stringifyStudents = JSON.stringify(array)
+        localStorage.setItem(key, stringifyStudents)
+    }
+
+}
+
+saveLocalList ('list', studentsList)
+
+
+
+function getLocalList(key){
+
+    var localList = localStorage.getItem(key)
+
+    if(localList === null) {
+
+        var array = []
+
+        return array
+    }
+
+}
+
+var studentsList = getLocalList('list')
+
+
+
+console.log(studentsList)
+
+
