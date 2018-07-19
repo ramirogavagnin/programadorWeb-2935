a2b3706e00cf569b804551c8dad73ddfc89d305f
Ramiro Gavagnin <ramirogavagnin@gmail.com>
2018-07-19 20:02:39 -0300

    add clase 16

--- Clase 15/Alumno/Ejercicio/js/index.js
+++ Clase 15/Alumno/Ejercicio/js/index.js
@@ -5,7 +5,7 @@ console.log('init ej 15')
 function getData (url, cbk) {
     $.ajax(url)
       .done(function (data) {
-        
+       
         cbk(null, data)
       
       })
@@ -13,25 +13,25 @@ function getData (url, cbk) {
         cbk(error)
       })
 
-
-
   }
 
   var urlBase = 'https://swapi.co/api/' + 'people/5/'
 
  function callBack (error, data){
 
-    var data = data
-
-    console.log(data.name + ' ' + data.height + ' ' + data.hair_color + ' ' + data.hair_color)
-
- }
-
- function showName (){
+    if(data){
 
     
+console.log(data.name + ' ' + data.gender)
+
+    }
+    else {
+
+      console.log('Error: ', error)
+    }
 
  }
+
 
   getData(urlBase, callBack)
 
