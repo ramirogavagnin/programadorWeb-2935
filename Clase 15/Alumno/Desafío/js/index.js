f788e86cbc0b56905a9d94a5409cf1a1e7fc08d3
Ramiro Gavagnin <ramirogavagnin@gmail.com>
2018-07-23 19:55:51 -0300

    desafío clase 15

--- Clase 15/Alumno/Desafío/js/index.js
+++ Clase 15/Alumno/Desafío/js/index.js
@@ -2,62 +2,36 @@ $(document).ready(function(){
 
 console.log('init desafío 15')
 
-getData(urlBase, getInfo)
-
-
 function getData (url, cbk) {
     $.ajax(url)
       .done(function (data) {
-      
         cbk(null, data)
-      
       })
       .fail(function (error) {
         cbk(error)
       })
   }
 
-
-  
-
 var urlBase = 'https://swapi.co/api/' + 'people/'
+  
+getData(urlBase, getInfo)
 
  function getInfo (error, data){
 
     if(data.results){
-
       showPeopleList (data.results)
      }
-
     if (data.next){
-
       getData(data.next, getInfo)
     }
-  
-
-    /* else {
-
-      console.log('Error: '+ error)
-    } */
-
  }
 
 
  function showPeopleList (results){
-  
-  var peopleList
-
   for (var i = 0; i < results.length; i++){
-
-peopleList = results[i]
-
-console.log('hola')
-
-   
+console.log(results[i].name)   
 }
-
  }
 
-  
 
 })
