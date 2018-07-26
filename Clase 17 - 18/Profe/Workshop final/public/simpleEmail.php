ca15cd9a61d42ca83154b610a1e28ab3822ff218
adrianc4 <adrian_c4@hotmail.com>
2018-07-26 20:28:29 -0300

    Add Clas final

--- Clase 17 - 18/Profe/Workshop final/public/simpleEmail.php
+++ Clase 17 - 18/Profe/Workshop final/public/simpleEmail.php
@@ -2,11 +2,14 @@
 
 $to = "adrian_c4@hotmail.com"; // This is your Email address
 $from = $_POST['email']; // This is the sender's Email address
-$first_name = $_POST['name']; // This is the sender's Name
+$first_name = $_POST['firstName']; // This is the sender's Name
 $subject = "Email de Star Wars API";
-$comments = $_POST['comments'];
+$comments = $first_name . ' te dijo: ' . $_POST['comments'];
 
 $headers = "From:" . $from;
+
+
+
 $email = mail($to,$subject,$comments,$headers);
 
 if(!$email){
